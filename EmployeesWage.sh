e145ac29e1b73b039667ca27f19b75969e3e92c1
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-04-19 06:56:42 +0530

    uc3 Branch

--- EmployeesWage.sh
+++ EmployeesWage.sh
@@ -1,15 +1,17 @@
-WagePerHour=20
-FullDayHour=9
+isFullTime=1
+isPartTime=2
+empHourRate=20
+randomCheck=$((RANDOM%3))
 
-empCheck=$((RANDOM%2))
-totalSalary=0
-
-if [ $empCheck -eq 1 ]
+if [ $isFullTime -eq $randomCheck ]; 
 then
-        echo "Employee is Present"
-        totalSalary=$(( WagePerHour * FullDayHour ))
+    empHrs=8
+elif [ $isPartTime -eq $randomCheck ]; 
+then
+    empHrs=4
 else
-        echo "Employee is Absent"
+    empHrs=0
 fi
 
-echo "Total Salary: $totalSalary"
+salary=$(($empHrs*$empHourRate))
+echo "Salary: " $salary
