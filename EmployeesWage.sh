43461752c26a32a85aacec4262f3e6f8f2bddb98
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-04-19 06:51:01 +0530

    uc2 Branch

--- EmployeesWage.sh
+++ EmployeesWage.sh
@@ -1,5 +1,15 @@
-if (( RANDOM % 2 == 1 )); then
-    echo "Employee is Present"
+WagePerHour=20
+FullDayHour=9
+
+empCheck=$((RANDOM%2))
+totalSalary=0
+
+if [ $empCheck -eq 1 ]
+then
+        echo "Employee is Present"
+        totalSalary=$(( WagePerHour * FullDayHour ))
 else
-    echo "Employee is Absent"
+        echo "Employee is Absent"
 fi
+
+echo "Total Salary: $totalSalary"
